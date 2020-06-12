48df174f8462cf89bfa094009a15b7104dfa7be8
mxschumacher <maximilianbschumacher@gmail.com>
2020-06-12 12:23:33 +0200

    Expand settings copying script

--- copy_settings_to_system.sh
+++ copy_settings_to_system.sh
@@ -2,3 +2,6 @@
 
 echo "Writing the fish config from this repo to the current host system"
 cp dotfiles/config.fish ~/.config/fish/config.fish
+
+echo "Copying the system maintance script to my home folder"
+cp scripts/system_maintenance ~
