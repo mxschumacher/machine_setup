14f7bb79540a39a8d4b70f4e024e986d6ab30023
mxschumacher <maximilianbschumacher@gmail.com>
2020-06-17 14:24:37 +0200

    add scripts to copy configuration into system and produce list of installed packages

--- copy_settings_to_system.sh
+++ copy_settings_to_system.sh
@@ -5,3 +5,6 @@ cp dotfiles/config.fish ~/.config/fish/config.fish
 
 echo "Copying the system maintance script to my home folder"
 cp scripts/system_maintenance ~
+
+echo "Copying the Polybar configs into the .config folder on the host"
+cp dotfiles/polybar/* ~/.config/polybar
---
+++ updating_system_packages.sh
@@ -0,0 +1,3 @@
+#!/usr/bin/bash
+
+yay -Qe > pacman_packages
