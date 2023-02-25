7e05c8801232c2dc56779f353be3b5d53973bbbe
mxschumacher <maximilianbschumacher@gmail.com>
2023-02-25 11:27:43 +0100

    add alacritty

--- copy_settings_to_system.sh
+++ copy_settings_to_system.sh
@@ -1,5 +1,8 @@
 #!/usr/bin/bash
 
+echo "Writing the i3 config from this repo to the current host system"
+cp dotfiles/config.i3_wm ~/.config/i3/config
+
 echo "Writing the fish config from this repo to the current host system"
 cp dotfiles/config.fish ~/.config/fish/config.fish
 
@@ -8,3 +11,6 @@ cp scripts/system_maintenance ~
 
 echo "Copying the Polybar configs into the .config folder on the host"
 cp dotfiles/polybar/* ~/.config/polybar
+
+echo "Copying the Alacritty configs into the .config folder on the host"
+cp dotfiles/alacritty/alacritty.yml ~/.config/alacritty/alacritty.yml
