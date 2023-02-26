bdeef5c5e1d28b3ff2a7f149cc3d2c44506a758a
mxschumacher <maximilianbschumacher@gmail.com>
2023-02-26 12:57:27 +0100

    add picom config

--- copy_settings_to_system.sh
+++ copy_settings_to_system.sh
@@ -1,5 +1,10 @@
 #!/usr/bin/bash
 
+# The individual configuration files live in this centralized repository. Each
+# program I use has a set of default locations where it is looking for the config files
+# so the copy_settings_to_system script copies them there.
+# Should I just use symlinks?
+
 echo "Writing the i3 config from this repo to the current host system"
 cp dotfiles/config.i3_wm ~/.config/i3/config
 
@@ -14,3 +19,6 @@ cp dotfiles/polybar/* ~/.config/polybar
 
 echo "Copying the Alacritty configs into the .config folder on the host"
 cp dotfiles/alacritty/alacritty.yml ~/.config/alacritty/alacritty.yml
+
+echo "Copying the Picom configs into the .config folder on the host"
+cp dotfiles/picom/picom.conf ~/.config/picom/picom.conf
